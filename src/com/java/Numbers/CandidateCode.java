ed3d4e97b0fe4cf7af0f6939454466f305e80f57
pratiks360 <pratiks360@gmail.com>
2020-04-30 09:07:53 +0530

    Packaged programs

--- src/com/java/Numbers/CandidateCode.java
+++ src/com/java/Numbers/CandidateCode.java
@@ -1,28 +1,26 @@
-/*
+package com.java.Numbers;/*
  * Enter your code here. Read input from STDIN. Print your output to STDOUT.
- * Your class should be named CandidateCode.
+ * Your class should be named com.java.Numbers.CandidateCode.
  */
 
-import java.io.*;
-import java.util.*;
 public class CandidateCode {
     public static void main(String args[]) throws Exception {
-        int len =Integer.parseInt(args[0]);
+        int len = Integer.parseInt(args[0]);
         String alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         char[] ap = alphabets.toCharArray();
-        for(int i=0;i<len;i++){
-           print1(ap[i],len);
+        for (int i = 0; i < len; i++) {
+            print1(ap[i], len);
 
-            if(i<len-1)
-            System.out.println("");
+            if (i < len - 1)
+                System.out.println("");
         }
 
     }
 
 
-    public static void print1(char a,int len) {
+    public static void print1(char a, int len) {
         for (int i = 0; i < len; i++) {
-            if(i!=0){
+            if (i != 0) {
                 System.out.print(" ");
 
             }
